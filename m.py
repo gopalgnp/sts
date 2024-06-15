f25ad9c7555d485f602f8e37281ccde2eab8b53b
gopalgnp <167167815+gopalgnp@users.noreply.github.com>
2024-06-15 16:02:12 +0530

    Update m.py

--- m.py
+++ m.py
@@ -236,7 +236,7 @@ def handle_bgmi(message):
             target = command[1]
             port = int(command[2])  # Convert time to integer
             time = int(command[3])  # Convert port to integer
-            if time > 240:
+            if time > 200:
                 response = "Error: Time interval must be less than 240."
             else:
                 record_command_logs(user_id, '/bgmi', target, port, time)
